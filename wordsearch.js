b2333ad9685da17d36a35477b662b909d8b34aa2
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-05 16:50:42 -0500

    Pair programming Selene & Suliman

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,11 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  return horizontalJoin.includes(word) ? true : false; // ternary operator
+};
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
+
+// Pair programming Selene Grossett & Suliman Fayyaz
